87a3256a320dd381d2102bc97af633456e008248
m.hiort <m.hiort@tu-berlin.de>
2023-12-19 13:06:11 +0100

    added comments and stop function

--- scripts/mininet_setup.py
+++ scripts/mininet_setup.py
@@ -1,18 +1,19 @@
 import os
 from time import sleep
-from mininet.net import Mininet as MN
+from mininet.net import Mininet
 from mininet.node import Controller
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
 
-class MininetNetworkManager:
+class MininetNetwork:
 
     def __init__(self):
         self.net = None
 
+    # Mininet setup with 2 hosts and 1 switch
     def mininet_2h_1s(self):
-        self.net = MN()
+        self.net = Mininet()
 
         self.net.addController('c0', controller=Controller)
 
@@ -26,8 +27,9 @@ class MininetNetworkManager:
         self.net.start()
         #CLI(self.net)
 
+    # Mininet setup with 1 host and 1 switch
     def mininet_1h_1s(self):
-        self.net = MN()
+        self.net = Mininet()
 
         self.net.addController('c0', controller=Controller)
     
@@ -39,20 +41,21 @@ class MininetNetworkManager:
         self.net.start()
         #CLI(self.net)
 
-
-    def cleanup_network(self):
+    def stop_mininet(self):
         if self.net:
             self.net.stop()
 
+    def cleanup_network(self):
         os.system('sudo mn -c')
         os.system('sudo fuser -k 6653/tcp')
 
 # This is for testing purposes:
 if __name__ == '__main__':
-
     setLogLevel('info')
-    mininet = MininetNetworkManager()
+
+    mininet = MininetNetwork()
     mininet.mininet_1h_1s()
-    mininet.mininet_2h_1s() 
+    #mininet.mininet_2h_1s() 
     sleep(5)
+    mininet.stop_mininet()
     mininet.cleanup_network()
