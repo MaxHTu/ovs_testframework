1ff8e17c8a133e10f363d3de208b0ba5ed805d47
m.hiort <m.hiort@tu-berlin.de>
2024-01-25 23:00:25 +0100

    new tests

--- tests.py
+++ tests.py
@@ -11,6 +11,8 @@ from mininet_setup import MininetNetwork
 def cve_2016_2074():
     setLogLevel('info')
 
+    ovs_config.set_log_path('cve_2016_2074')
+
     mininet = MininetNetwork()
     mininet.mininet_1h_1s()
    
@@ -26,6 +28,7 @@ def cve_2016_2074():
         vulnerable = True
 
     mininet.cleanup_network()
+    ovs_config.reset_log_path()
 
     return vulnerable
 
@@ -63,14 +66,33 @@ def cve_2016_10377():
 
 def cve_2017_9264():
     setLogLevel('info')
+    os.system('gcc packets/cve_2017_9264.c -o packets/cve_2017_9264')
+
+    log_path = ovs_config.set_log_path('cve_2017_9264')
+
+    mininet = MininetNetwork()
+    mininet.mininet_2h_1s()
+
+    #mininet.net['h2'].cmd('sudo wireshark -i h2-eth0 -k &')
+    #sleep(5)
+    mininet.net['h1'].cmd('./packets/cve_2017_9264')
+    #sleep(10)
+
+    #mininet.net['h2'].cmd('sudo killall wireshark')
+    mininet.stop_mininet()
     
-    #mininet = MininetNetwork()
-    #mininet.mininet_2h_1s()
-
-    #mininet.stop_mininet()
-    #mininet.cleanup_network()
-
     vulnerable = False
+    with open(log_path, 'r') as log_file:
+        log_content = log_file.read()
+        error = re.compile(r'failed to put\[create\] \(Invalid argument\)')
+        if error.search(log_content):
+            vulnerable = True
+
+    mininet.cleanup_network()
+    ovs_config.reset_log_path()
+
+    os.system('rm packets/cve_2017_9264')
+
     return vulnerable
 
 def cve_2020_27827():
@@ -92,8 +114,8 @@ def cve_2020_35498():
     mininet = MininetNetwork()
     mininet.mininet_2h_1s()
 
-    mininet.net['s1'].cmd('sudo ovs-ofctl add-flow s1 priority=5,ip,ip_dst=10.0.0.1,actions=1')
-    mininet.net['s1'].cmd('sudo ovs-ofctl add-flow s1 priority=5,ip,ip_dst=10.0.0.2,actions=2')
+    mininet.net['s1'].cmd('sudo ovs-ofctl add-flow s1 priority=10,ip,ip_dst=10.0.0.1,actions=1')
+    mininet.net['s1'].cmd('sudo ovs-ofctl add-flow s1 priority=10,ip,ip_dst=10.0.0.2,actions=2')
     mininet.net['s1'].cmd('sudo ovs-ofctl add-flow s1 priority=0,actions=drop')
     #mininet.net['h1'].cmd('arp -s 10.0.0.2 00:00:00:00:00:02')
     #mininet.net['h2'].cmd('arp -s 10.0.0.1 00:00:00:00:00:01')
@@ -110,11 +132,11 @@ def cve_2020_35498():
     mininet.stop_mininet()
 
     vulnerable = False
-    tcpdump_output = subprocess.check_output(['tcpdump', '-r', cve_pcap_filename, '-n'], text=True)
+    tcpdump_output = subprocess.check_output(['tcpdump', '-r', cve_pcap_filename, '-n']).decode('utf-8')
     packet = re.compile(r'10.0.0.1 > 10.0.0.2:*')
     if not packet.search(tcpdump_output):
         vulnerable = True
-
+        
     mininet.cleanup_network()
 
     os.system('rm packets/cve_2020_35498')
@@ -151,6 +173,7 @@ def cve_2022_4338():
 
 def cve_2022_32166():
     os.system('ovs-vsctl add-br br-int')
+    
     os.system('ovs-ofctl add-flow br-int "table=0,cookie=0x1234,priority=10000,icmp,actions=drop"')
     os.system('ovs-ofctl --strict del-flows br-int "table=0,cookie=0x1234/-1,priority=10000"')
 
@@ -165,11 +188,5 @@ def cve_2023_1668():
 
 # This is for testing purposes:
 if __name__ == '__main__':
-    #test1 = cve_2016_2074()
-    #test2 = cve_2016_10377()
-    test = cve_2020_35498()
+    test = cve_2017_9264()
     print(test)
-    #cve_2022_32166()
-    #print(test1)
-    #print(test2)
-  
